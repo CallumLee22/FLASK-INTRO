304ad2b33590726752fedf96a5778f12f12bef58
Callum Lee <callum.lee@stfc.ac.uk>
2023-09-12 14:28:26 +0100

    calculate sum

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+@app.route("/", methods=["POST", "GET"])
+def form():
+  if request.method == "GET":
+    with app.app_context():
+      return render_template("calc.jinja")
+  
+  elif request.method == "POST":
+    first_name = request.form["fname"]
+    first_name_letters = list(first_name)
+    sum = 0
+    
+    for letter in first_name_letters:
+      sum += ord(letter)
+    
+    last_name = request.form["lname"]
+    last_name_letters = list(last_name)
+
+    for letter in last_name_letters:
+      sum += ord(letter)
+    return sum
+
+@app.route("/display_sum")
+def display_sum():
   with app.app_context():
-        return render_template("calc.jinja")
+      return render_template("display_sum.jinja")

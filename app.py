1d184691146c2f6a0c3855d05029c2d5c1689681
Callum Lee <callum.lee@stfc.ac.uk>
2023-09-12 10:29:05 +0100

    Shows form for entering name

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import flask
-app = flask.Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  with app.app_context():
+        return render_template("calc.jinja")
